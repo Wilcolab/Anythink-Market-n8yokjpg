1032bfd2e26027875f5559700932bf6f4101ebba
Rishabh Varshney <65810355+var-rishabh@users.noreply.github.com>
2023-03-07 13:40:20 +0000

    seeding completed

--- backend/app.js
+++ backend/app.js
@@ -55,6 +55,55 @@ require("./config/passport");
 
 app.use(require("./routes"));
 
+const User = mongoose.model("User");
+const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
+
+
+async function seedDatabase() {
+  for (let i = 0; i < 100; i++) {
+    // add user
+    const user = { username: `user${i}`, email: `user${i}@gmail.com` };
+    const options = { upsert: true, new: true };
+    const createdUser = await User.findOneAndUpdate(user, {}, options);
+    
+    // add item to user
+    const item = {
+      slug: `slug${i}`,
+      title: `title ${i}`,
+      description: `description ${i}`,
+      seller: createdUser,
+    };
+    const createdItem = await Item.findOneAndUpdate(item, {}, options);
+    
+    // add comments to item
+    if (!createdItem?.comments?.length) {
+      let commentIds = [];
+      for (let j = 0; j < 100; j++) {
+        const comment = new Comment({
+          body: `body ${j}`,
+          seller: createdUser,
+          item: createdItem,
+        });
+        await comment.save();
+        commentIds.push(comment._id);
+      }
+      createdItem.comments = commentIds;
+      await createdItem.save();
+    }
+  }
+}
+
+seedDatabase()
+  .then(() => {
+  console.log("Finished DB seeding");
+  process.exit(0);
+})
+.catch((err) => {
+  console.log(`Error while running DB seed: ${err.message}`);
+  process.exit(1);
+});
+
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   if (req.url === "/favicon.ico") {
